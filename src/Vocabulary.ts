e8b307051cb5de002c752f8835d048b7bc8ca8b7
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-01-26 15:21:17 +0900

    add vocabulary type

---
+++ src/Vocabulary.ts
@@ -0,0 +1,11 @@
+export type Vocabulary = {
+  word: string,
+  pronunciation: string,
+  meanings: string[],
+  text: string,
+}
+
+export type ExampleSentence = {
+  sentence: string,
+  meaning: string
+}

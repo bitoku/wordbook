72b0d33d1b3cae46a900502163bd5c261e83c12b
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-01-26 17:17:43 +0900

    separate vocab from question

--- src/Vocabulary.ts
+++ src/Vocabulary.ts
@@ -1,9 +1,14 @@
 export type Vocabulary = {
   word: string,
   pronunciation: string,
+  syllable: string,
+  pos: PoS,
+  inflection: string,
   meanings: string[],
   text: string,
 }
+
+export type PoS = string; // TODO: part of speech
 
 export type ExampleSentence = {
   sentence: string,
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,19 +2,37 @@ import {Body, Button, Card, CardItem, Text} from "native-base";
 import {styles} from "../style/style";
 import {Animated, View} from "react-native";
 import React, {useState} from "react";
+import {Vocabulary} from "../Vocabulary";
 
 export const Question = () => {
   const [userAns, setUserAns] = useState<number>(0);
-  const options = ['りんご', 'みかん', 'ぶどう', 'レモン'];
+  const vocab: Vocabulary = {
+    word: 'apple',
+    pronunciation: 'ǽpl',
+    syllable: 'ap･ple',
+    pos: 'noun',
+    inflection: '複～s | -z |',
+    meanings: ['りんご'],
+    text: `1 Cリンゴ; リンゴの木(apple tree) (!果肉ではU)
+  ▸ bake [bite into] an apple
+  リンゴを焼く[かじる]
+  ▸ An apple a day keeps the doctor away.
+  〘ことわざ〙 1日にリンゴ1つで医者いらず (!リンゴは健康に良いとされる) .
+  2 Cリンゴに似た果実; リンゴに形が似たもの
+  ▸ a crab apple
+  野生リンゴ.
+  3 〖A-〗アップル〘米国のIT企業〙; ＝ Macintosh.`
+  }
+  const options = [vocab.meanings[0], 'みかん', 'ぶどう', 'レモン'];
   const correctAns = 1;
 
   return (
     <View style={styles.question}>
       <Text style={[styles.common, styles.word]}>
-        apple
+        {vocab.word}
       </Text>
       <Text style={[styles.common, styles.phonetic]}>
-        [ǽpl]
+        [ {vocab.pronunciation} ]
       </Text>
       {
         options.map((option, idx) => {
@@ -45,26 +63,18 @@ export const Question = () => {
         !!userAns &&
         <Card style={[styles.common]}>
           <CardItem header>
-            <Text style={[styles.common]}>ap･ple</Text>
-            <Text style={[styles.common]}>| ǽp(ə)l |</Text>
+            <Text style={[styles.common]}>{vocab.syllable}</Text>
+            <Text style={[styles.common]}>| {vocab.pronunciation} |</Text>
           </CardItem>
           <CardItem>
             <Text style={[styles.common]}>
-              名詞複～s | -z |
+              {vocab.pos} {vocab.inflection}
             </Text>
           </CardItem>
           <CardItem>
             <Body>
               <Text style={[styles.common]}>
-                1 Cリンゴ; リンゴの木(apple tree) (!果肉ではU)
-                ▸ bake [bite into] an apple
-                リンゴを焼く[かじる]
-                ▸ An apple a day keeps the doctor away.
-                〘ことわざ〙 1日にリンゴ1つで医者いらず (!リンゴは健康に良いとされる) .
-                2 Cリンゴに似た果実; リンゴに形が似たもの
-                ▸ a crab apple
-                野生リンゴ.
-                3 〖A-〗アップル〘米国のIT企業〙; ＝ Macintosh.
+                {vocab.text}
               </Text>
             </Body>
           </CardItem>
